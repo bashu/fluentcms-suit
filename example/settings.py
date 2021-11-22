e0ba368c4a055329cbd4d6cfff84838fb1fa103c
Basil Shubin <basil.shubin@gmail.com>
2021-11-22 16:19:05 +0700

    Formatting (black)

--- example/settings.py
+++ example/settings.py
@@ -7,7 +7,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os, re
+import os
+import re
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -18,7 +19,7 @@ from django.utils.translation import gettext_lazy as _
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'YOUR_SECRET_KEY'
+SECRET_KEY = "YOUR_SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,117 +31,112 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.sites',
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
-    'django.contrib.contenttypes',
-    'django.contrib.messages',
-
-    'suit',
-    'fluentcms_suit',
-    'django.contrib.admin',
-
-    'fluent_pages',
-    'fluent_pages.pagetypes.flatpage',
-    'fluent_pages.pagetypes.fluentpage',
-    'fluent_pages.pagetypes.redirectnode',
-    'fluent_pages.pagetypes.textfile',
-
-    'mptt',
-    'polymorphic',
-    'polymorphic_tree',
-    'slug_preview',
-    'parler',
-
-    'fluent_contents',
-    'fluent_contents.plugins.code',
-    'fluent_contents.plugins.commentsarea',
-    'fluent_contents.plugins.gist',
-    'fluent_contents.plugins.googledocsviewer',
-    'fluent_contents.plugins.iframe',
-    'fluent_contents.plugins.markup',
-    'fluent_contents.plugins.oembeditem',
-    'fluent_contents.plugins.picture',
-    'fluent_contents.plugins.rawhtml',
-    'fluent_contents.plugins.sharedcontent',
-    'fluent_contents.plugins.text',
-
-    'any_urlfield',
-    'any_imagefield',
-
-    'django_wysiwyg',
-    'tinymce',
+    "django.contrib.auth",
+    "django.contrib.sites",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "suit",
+    "fluentcms_suit",
+    "django.contrib.admin",
+    "fluent_pages",
+    "fluent_pages.pagetypes.flatpage",
+    "fluent_pages.pagetypes.fluentpage",
+    "fluent_pages.pagetypes.redirectnode",
+    "fluent_pages.pagetypes.textfile",
+    "mptt",
+    "polymorphic",
+    "polymorphic_tree",
+    "slug_preview",
+    "parler",
+    "fluent_contents",
+    "fluent_contents.plugins.code",
+    "fluent_contents.plugins.commentsarea",
+    "fluent_contents.plugins.gist",
+    "fluent_contents.plugins.googledocsviewer",
+    "fluent_contents.plugins.iframe",
+    "fluent_contents.plugins.markup",
+    "fluent_contents.plugins.oembeditem",
+    "fluent_contents.plugins.picture",
+    "fluent_contents.plugins.rawhtml",
+    "fluent_contents.plugins.sharedcontent",
+    "fluent_contents.plugins.text",
+    "any_urlfield",
+    "any_imagefield",
+    "django_wysiwyg",
+    "tinymce",
 ]
 
 import django
-if 'fluent_contents.plugins.commentsarea' in INSTALLED_APPS:
+
+if "fluent_contents.plugins.commentsarea" in INSTALLED_APPS:
     if django.VERSION < (1, 7):
         INSTALLED_APPS += [
-            'django.contrib.comments',
+            "django.contrib.comments",
         ]
     else:
         INSTALLED_APPS += [
-            'django_comments',
+            "django_comments",
         ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'example.urls'
+ROOT_URLCONF = "example.urls"
 
 SITE_ID = 1
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(os.path.dirname(__file__), 'templates'),
-            os.path.join(os.path.dirname(__file__), 'templates', 'layouts'),
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(os.path.dirname(__file__), "templates"),
+            os.path.join(os.path.dirname(__file__), "templates", "layouts"),
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'example.wsgi.application'
+WSGI_APPLICATION = "example.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = "en"
 LANGUAGES = (
-    ('en', _('English')),
-    ('es', _('Spanish')),
-    ('fr', _('French')),
+    ("en", _("English")),
+    ("es", _("Spanish")),
+    ("fr", _("French")),
 )
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -152,27 +148,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 
 # Absolute path to the directory static files should be collected to.
-STATIC_ROOT = os.path.join(os.path.dirname(__file__), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), "static")
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-FLUENT_PAGES_TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates', 'layouts')
-FLUENT_PAGES_PARENT_ADMIN_MIXIN = 'fluentcms_suit.admin.FluentPagesParentAdminMixin'
+FLUENT_PAGES_TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "templates", "layouts")
+FLUENT_PAGES_PARENT_ADMIN_MIXIN = "fluentcms_suit.admin.FluentPagesParentAdminMixin"
 
 DJANGO_WYSIWYG_FLAVOR = "tinymce"
 
 PARLER_DEFAULT_LANGUAGE = LANGUAGE_CODE
 PARLER_LANGUAGES = {
-    SITE_ID: tuple({'code': lang[0]} for lang in LANGUAGES),
-    'default': {
-        'fallback': LANGUAGE_CODE,
-        'hide_untranslated': False,
-    }
+    SITE_ID: tuple({"code": lang[0]} for lang in LANGUAGES),
+    "default": {
+        "fallback": LANGUAGE_CODE,
+        "hide_untranslated": False,
+    },
 }
--- fluentcms_suit/admin.py
+++ fluentcms_suit/admin.py
@@ -1,34 +1,41 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
 class SuitAdminMixin:
-
     def formfield_for_dbfield(self, db_field, **kwargs):
-        if 'suit' in settings.INSTALLED_APPS:
-            from suit.widgets import SuitSplitDateTimeWidget, SuitDateWidget
+        if "suit" in settings.INSTALLED_APPS:
+            from suit.widgets import SuitDateWidget, SuitSplitDateTimeWidget
 
             if isinstance(db_field, models.DateTimeField):
-                kwargs.setdefault('widget', SuitSplitDateTimeWidget)
+                kwargs.setdefault("widget", SuitSplitDateTimeWidget)
             elif isinstance(db_field, models.DateField):
-                kwargs.setdefault('widget', SuitDateWidget)
+                kwargs.setdefault("widget", SuitDateWidget)
         return super().formfield_for_dbfield(db_field, **kwargs)
 
-    
-class FluentPagesParentAdminMixin:
 
+class FluentPagesParentAdminMixin:
     def get_action_icons(self, node):
         actions = []
         if node.can_have_children:
             actions.append(
-                """<a href="add/?{parent_attr}={id}" title="{title}" class="add-child-object"><i class="icon-plus-sign icon-alpha75"></i></a>""".format(parent_attr=self.model._mptt_meta.parent_attr, id=node.pk, title=_('Add sub node'), static=settings.STATIC_URL))
+                """<a href="add/?{parent_attr}={id}" title="{title}" class="add-child-object"><i class="icon-plus-sign icon-alpha75"></i></a>""".format(
+                    parent_attr=self.model._mptt_meta.parent_attr,
+                    id=node.pk,
+                    title=_("Add sub node"),
+                    static=settings.STATIC_URL,
+                )
+            )
         else:
-            actions.append(self.EMPTY_ACTION_ICON.format(STATIC_URL=settings.STATIC_URL, css_class='add-child-object'))
+            actions.append(self.EMPTY_ACTION_ICON.format(STATIC_URL=settings.STATIC_URL, css_class="add-child-object"))
 
         if self.can_preview_object(node):
             actions.append(
-                """<a href="{url}" title="{title}" target="_blank" class="viewsitelink"><i class="icon-globe icon-alpha75"></i></a>""".format(url=node.get_absolute_url(), title=_('View on site'), static=settings.STATIC_URL))
+                """<a href="{url}" title="{title}" target="_blank" class="viewsitelink"><i class="icon-globe icon-alpha75"></i></a>""".format(
+                    url=node.get_absolute_url(), title=_("View on site"), static=settings.STATIC_URL
+                )
+            )
 
         # The is_first_sibling and is_last_sibling is quite heavy. Instead rely on CSS to hide the arrows.
         move_up = f'<a href="{node.pk}/move_up/" class="move-up">\u2191</a>'
